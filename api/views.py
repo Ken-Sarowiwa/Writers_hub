e8210cd6f7c976da3f79cc38c4c7466f3b1cca49
ken sarowiwa <ken.wandolah@yahoo.com>
2022-09-12 23:11:03 +0300

    Project set up and initial commit

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Profile, Articles
+from rest_framework import viewsets, permissions
+from .serializers import ProfileSerializer, ArticleSerializer
 # Create your views here.
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
