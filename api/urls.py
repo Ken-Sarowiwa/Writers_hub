971e05104317cef2cb177c94328f07b387733c49
ken sarowiwa <ken.wandolah@yahoo.com>
2022-09-13 22:05:35 +0300

    urls

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from django.contrib import admin
 from rest_framework import routers
 from api import views
-from .views import UserViewSet, GroupViewSet, ArticlesViewSet
+from .views import UserViewSet, GroupViewSet, ArticlesViewSet, ProfileViewSet
 
 router = routers.DefaultRouter()
 
@@ -12,6 +12,8 @@ router.register(r"profiles", views.ProfileViewSet)
 
 url patterns = [
     path("" , include(router.urls)),
-    path("api/articles", ArticlesViewSet.as_view({"get": "list"}))
+    path("api/articles", ArticlesViewSet.as_view({"get": "list"})),
+    path("api/profiles", ProfileViewSet.as_view({"get": "list"})),
+    path("api/users", UserViewSet.as_view({"get":"list"})),
 
 ]
