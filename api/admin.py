94fb4877266e58e4b2e8ce1a48a06a8fad84d85a
ken sarowiwa <ken.wandolah@yahoo.com>
2022-09-14 15:44:53 +0300

    admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Articles, Profile
 # Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("name", "nickname", "email", "bio", "date_of_birth")
+    list_filter = ("name", "nickname", "email", "bio", "date_of_birth")
+    list_per_page = 10
+    search_fields = ("name", "nickname", "email", "bio", "date_of_birth")
