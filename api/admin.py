756e2affcbb5edc3bf52c34ea8fdb65a20448235
ken sarowiwa <ken.wandolah@yahoo.com>
2022-09-15 07:50:31 +0300

    admin

--- api/admin.py
+++ api/admin.py
@@ -2,8 +2,23 @@ from django.contrib import admin
 from .models import Articles, Profile
 # Register your models here.
 
+
+
+
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ("name", "nickname", "email", "bio", "date_of_birth")
     list_filter = ("name", "nickname", "email", "bio", "date_of_birth")
     list_per_page = 10
     search_fields = ("name", "nickname", "email", "bio", "date_of_birth")
+    ordering = ("name", "nickname", "email", "bio", "date_of_birth")
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title", "content", "author", "date", "image", "comment", "share")
+    list_filter = ("title", "content", "author", "date", "image", "likes", "comment", "share")
+    list_per_page = 10
+    search_fields = ("title", "content", "author", "date", "image", "likes", "comment", "share")
+    ordering = ("title", "content", "author", "date", "image", "likes", "comment", "share")
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Articles, ArticleAdmin)
